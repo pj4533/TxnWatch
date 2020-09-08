b1ffea17863922e633656e5fcd6abdfc54a2247c
PJ Gray <pj@evertrue.com>
2020-09-08 11:31:36 -0400

    Compiles with web3

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "TxnWatch",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .executable(name: "txnwatch", targets: ["TxnWatch"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-        .package(url: "https://github.com/matter-labs/web3swift", from: "2.2.1")
+        .package(name: "Web3swift", url: "https://github.com/matter-labs/web3swift", .branch("2.2.1")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.0.0"))
     ],
     targets: [
         .target(name: "TxnWatch", dependencies: [
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            .product(name: "Web3swift", package: "web3swift")
+            .product(name: "web3swift", package: "Web3swift"),
+            .product(name: "CryptoSwift", package: "CryptoSwift")
         ])
     ]
 )
