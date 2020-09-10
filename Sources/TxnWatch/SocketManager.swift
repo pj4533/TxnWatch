3b1d27faa4e53c283bc2911463104b96b79a0064
PJ Gray <pj@evertrue.com>
2020-09-10 18:25:49 -0400

    Initial work decoding data

--- Sources/TxnWatch/SocketManager.swift
+++ Sources/TxnWatch/SocketManager.swift
@@ -1,7 +1,8 @@
 import Foundation
 import Starscream
-
-
+import Web3
+import Web3PromiseKit
+import Web3ContractABI
 
 class SocketManager : WebSocketDelegate {
     
@@ -18,23 +19,21 @@ class SocketManager : WebSocketDelegate {
     }
     
     func getTxns(forAddress address: String) {
-                // debugging
-        //        let txnHash = "0xf4e646e0b7951dc7fdfc570b3d4ce734b9969914099692833516409d36e37b6a"
-        //        self.printTransactionHash(txnHash)
-
         let uniswapDataSource = UniswapDataSource()
         uniswapDataSource.getToken(tokenId: address, withSuccess: { (token) in
             self.token = token
             if let token = token {
                 print("\nWatching transactions for \(token.name) (\(token.symbol))\n")
-                print("\("Date".white.bold)\t\t\t\("Type".white.bold)\t\("Price (USD)".white.bold)\t\("Price (ETH)".white.bold)\t\("Amount \(token.symbol)".white.bold)\t\("Total ETH".white.bold)\t\("Maker".white.bold)")
-                
+                print("\("Date".white.bold)\t\t\t\("Type".white.bold)\t\("Price (USD)".white.bold)\t\("Price (ETH)".white.bold)\t\("Amount \(token.symbol)".white.bold)\t\("Total ETH".white.bold)\t\("TXN".white.bold)")
+
                 let socketString = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_subscribe\",\"id\":1,\"params\":[\"logs\", {\"address\":[\"\(address)\"]}]}"
                 if self.debug {
                     print("--> \(socketString)")
                 }
                 self.socket?.write(string: socketString, completion: nil)
-
+                
+                // error case:  tokens for tokens when target is first part of swap not last (ie tokens for tokens SELL, not BUY)
+                // 0xc555a242ddddaf11d3d8a0d13f8cb321ac65bbfc2f0bee7ca608c3ee16d1cc2a
             } else {
                 print("Token returned nil")
             }
@@ -47,30 +46,155 @@ class SocketManager : WebSocketDelegate {
         if (self.debug) {
             print("--> printTransactionHash()   \(txnHash)")
         }
-        let datasource = InfuraDataSource()
-        datasource.getTxnHash(txnHash: txnHash, withSuccess: { (transaction) in
-            let coinGeckoDataSource = CoinGeckoDataSource()
-            coinGeckoDataSource.getETHPrice(withSuccess: { (ethPrice) in
-                if let valueDecimal = transaction?.value?.hexaToDecimal, valueDecimal > 0 {
-                    let decimals = self.token?.decimals ?? "0"
-                    let doubleValue = Double("1e-\(decimals)") ?? 0
-                    let totalEth = Double(valueDecimal) * (doubleValue)
-                    let priceUSD = self.token?.usdMarketPrice(withEtherPrice: ethPrice) ?? 0.0
-                    let priceETH = self.token?.derivedETH
-                    var amountToken = 0.0
-                    if let priceETH = priceETH, let priceETHDouble = Double(priceETH) {
-                        amountToken = totalEth / priceETHDouble
-                        print(String(format: "<somedate>\t\t\("buy".green)\t%.8f\t%.8f\t%.8f\t%.8f\t\(transaction?.from ?? "???")",priceUSD,priceETHDouble,amountToken,totalEth))
-                    }
-                } else {
-                    print(String(format: "<somedate>\t\t\("sell?".red)\t%.8f\t%.8f\t%.8f\t%.8f\t\(transaction?.from ?? "???")",0.0,0.0,0.0,0.0))
-                }
-            }) { (error) in
-                print("Coin Gecko Error: \(error?.localizedDescription ?? "Unknown Error")")
-            }
-        }) { (error) in
-            print(error?.localizedDescription ?? "Unknown Error")
+        enum TransactionType : String {
+            case buy, sell
         }
+        var transactionType : TransactionType = .buy
+        var inAmount = 0.0
+        var outAmount = 0.0
+        let decimals = self.token?.decimals ?? "0"
+        let decimalMultiplier = Double("1e-\(decimals)") ?? 0
+        var foundSwap = false
+        if let tokenId = self.token?.id.lowercased() {
+            do {
+                let web3 = Web3(rpcURL: "https://mainnet.infura.io/v3/\(Secrets().infuraProjectId)")
+                let contractAddress = try EthereumAddress(hex: tokenId, eip55: false)
+                let contractAddressChecksummed = try EthereumAddress(hex: contractAddress.hex(eip55: true), eip55: true)
+                let contract = web3.eth.Contract(type: GenericERC20Contract.self, address: contractAddressChecksummed)
+                let txn = EthereumValue(stringLiteral: txnHash)
+                try contract.eth.getTransactionByHash(blockHash: EthereumData(EthereumData(ethereumValue: txn.ethereumValue())), response: { (txnObject) in
+
+                    let inputDataMethodSig = txnObject.result??.input.hex().prefix(10)
+//                    let inputDataParams = String(txnObject.result??.input.hex().dropFirst(10) ?? "")
+                    
+                    // swapExactTokensForETH
+                    if inputDataMethodSig == "0x18cbafe5" {
+                        transactionType = .sell
+                    }
+                    
+                    // swapExactETHForTokens
+                    if inputDataMethodSig == "0x7ff36ab5" {
+                        transactionType = .buy
+//                        do {
+//                            let params = try ABI.decodeParameters(types: [.uint256,.array(type: .address, length: 2),.address,.uint256], from: inputDataParams)
+//                            print("Input Data: \(params)")
+//                        } catch let error {
+//                            print(error)
+//                        }
+                    }
+
+                    // swapExactTokensForTokens
+                    if inputDataMethodSig == "0x38ed1739" {
+                        transactionType = .buy
+//                        do {
+//                            let params = try ABI.decodeParameters(types: [.uint256,.uint256,.array(type: .address, length: 3),.address,.uint256], from: inputDataParams)
+//                            print("Input Data: \(params)")
+//                        } catch let error {
+//                            print(error)
+//                        }
+                    }
+                    
+                    do {
+                        try contract.eth.getTransactionReceipt(transactionHash:
+                            EthereumData(EthereumData(ethereumValue: txn.ethereumValue())), response: { (receipt) in
+                                var logIndex = 1
+                                for log in receipt.result??.logs.reversed() ?? [] {
+                                    if self.debug {
+                                        print("Log #\(logIndex) Address: \(log.address.hex(eip55: false))")
+                                        print("Log #\(logIndex) Data: \(log.data.ethereumValue().string ?? "???")")
+                                    }
+
+                                    // ERC20 Sig for Swap
+                                    // Swap (index_topic_1 address sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, index_topic_2 address to)
+                                    //
+                                    if (log.topics.first?.hex() == "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822") {
+                                        foundSwap = true
+                                        do {
+                                            let param = try ABI.decodeParameters(types: [.uint256,.uint256,.uint256,.uint256], from: log.data.hex())
+                                            if self.debug {
+                                                print("Log #\(logIndex) Decoded Data: \(param)")
+                                            }
+
+                                            if let amounts = param as? [BigUInt] {
+                                                var amountIndex = 0
+                                                for amount in amounts {
+                                                    if amount != 0, (amountIndex == 0) || (amountIndex == 1) {
+                                                        inAmount = Double(amount) * decimalMultiplier
+                                                    }
+                                                    if amount != 0, (amountIndex == 2) || (amountIndex == 3) {
+                                                        outAmount = Double(amount) * decimalMultiplier
+                                                    }
+                                                    amountIndex = amountIndex + 1
+                                                }
+                                            }
+                                        } catch let error {
+                                            print(error)
+                                        }
+                                    }
+
+                                    if self.debug {
+                                        var topicIndex = 1
+                                        for topic in log.topics {
+                                            print("Log #\(logIndex) Topic #\(topicIndex): \(topic.hex())")
+                                            topicIndex = topicIndex + 1
+                                        }
+                                    }
+                                    logIndex = logIndex + 1
+
+                                    // if we found a swap, there might be another if it is a swapTokensForTokens, but we just care about the first one (in reverse order)
+                                    if foundSwap {
+                                        break
+                                    }
+                                }
+
+                                if foundSwap {
+                                    let coinGeckoDataSource = CoinGeckoDataSource()
+                                    coinGeckoDataSource.getETHPrice(withSuccess: { (ethPrice) in
+                                        let totalEth = transactionType == .sell ? outAmount : inAmount
+                                        let priceUSD = self.token?.usdMarketPrice(withEtherPrice: ethPrice) ?? 0.0
+                                        let priceETH = self.token?.derivedETH
+                                        let amountToken = transactionType == .buy ? outAmount : inAmount
+                                        let colorizedTransactionType = transactionType == .buy ? transactionType.rawValue.green : transactionType.rawValue.red
+                                        print(String(format: "<somedate>\t\t\(colorizedTransactionType)\t%.8f\t%.8f\t%.8f\t%.8f\t\(txnHash)",priceUSD,Double(priceETH ?? "0.0") ?? 0.0,amountToken,totalEth))
+                                    }) { (error) in
+                                        print("Coin Gecko Error: \(error?.localizedDescription ?? "Unknown Error")")
+                                    }
+                                }
+                        })
+
+                    } catch let error {
+                       print(error)
+                    }
+                })
+            } catch let error {
+                print(error)
+            }
+        }
+
+//        let datasource = InfuraDataSource()
+//        datasource.getTxnHash(txnHash: txnHash, withSuccess: { (transaction) in
+//            let coinGeckoDataSource = CoinGeckoDataSource()
+//            coinGeckoDataSource.getETHPrice(withSuccess: { (ethPrice) in
+//                if let valueDecimal = transaction?.value?.hexaToDecimal, valueDecimal > 0 {
+//                    let decimals = self.token?.decimals ?? "0"
+//                    let doubleValue = Double("1e-\(decimals)") ?? 0
+//                    let totalEth = Double(valueDecimal) * (doubleValue)
+//                    let priceUSD = self.token?.usdMarketPrice(withEtherPrice: ethPrice) ?? 0.0
+//                    let priceETH = self.token?.derivedETH
+//                    var amountToken = 0.0
+//                    if let priceETH = priceETH, let priceETHDouble = Double(priceETH) {
+//                        amountToken = totalEth / priceETHDouble
+//                        print(String(format: "<somedate>\t\t\("buy".green)\t%.8f\t%.8f\t%.8f\t%.8f\t\(transaction?.from ?? "???")",priceUSD,priceETHDouble,amountToken,totalEth))
+//                    }
+//                } else {
+//                    print(String(format: "<somedate>\t\t\("sell?".red)\t%.8f\t%.8f\t%.8f\t%.8f\t\(transaction?.from ?? "???")",0.0,0.0,0.0,0.0))
+//                }
+//            }) { (error) in
+//                print("Coin Gecko Error: \(error?.localizedDescription ?? "Unknown Error")")
+//            }
+//        }) { (error) in
+//            print(error?.localizedDescription ?? "Unknown Error")
+//        }
     }
     
     func processText(_ text:String) {
